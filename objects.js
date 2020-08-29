b14597b864e7294b83756ef23656b6d2572cafdc
mianamir <mianamirlahore@gmail.com>
2020-08-29 16:19:17 +0500

    Js objects

---
+++ objects.js
@@ -0,0 +1,20 @@
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+
+  var person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+var x = new String();        // Declares x as a String object
+var y = new Number();        // Declares y as a Number object
+var z = new Boolean();       // Declares z as a Boolean object
+
